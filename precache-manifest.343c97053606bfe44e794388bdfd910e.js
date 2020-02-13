e05dd579045d297063e48c47e2b17e40b330fb5c
RomanLev4yk <romanlev4yk@ukr.net>
2020-02-13 17:06:58 +0200

    Updates

---
+++ precache-manifest.343c97053606bfe44e794388bdfd910e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd0ad0ab67f652018a115b7555f4ed5d",
+    "url": "/socialNet/index.html"
+  },
+  {
+    "revision": "3ec63c81fac093f5f970",
+    "url": "/socialNet/static/css/main.f78df48c.chunk.css"
+  },
+  {
+    "revision": "b0f7febc05c47ba81e35",
+    "url": "/socialNet/static/js/2.c33bb04c.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/socialNet/static/js/2.c33bb04c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ec63c81fac093f5f970",
+    "url": "/socialNet/static/js/main.629130fd.chunk.js"
+  },
+  {
+    "revision": "c50c91c570a7c63554aa",
+    "url": "/socialNet/static/js/runtime-main.b4435621.js"
+  },
+  {
+    "revision": "9bef6277eba9b2df8a0d22718d948f91",
+    "url": "/socialNet/static/media/login.9bef6277.jpg"
+  }
+]);
